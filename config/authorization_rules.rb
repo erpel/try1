f20fa174c5f2b345e1686c6901f1fba0081fafff
erpel <philipp@copythat.de>
2010-02-08 11:46:55 +0100

    neue rollen

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -1,10 +1,14 @@
 authorization do 
-	role :admin do
+	role :superuser do
 		has_permission_on [:import_assets], :to => [:index, :csv_import]
 		has_permission_on [:tickets,:assets, :roles], :to => [:index, :show, :new, :create, :edit, :update, :destroy]
 	end
+	role :admin do
+		has_permission_on [:import_assets], :to => [:index, :csv_import]
+		has_permission_on [:tickets,:assets], :to => [:index, :show, :new, :create, :edit, :update, :destroy]
+	end
 	
-	role :accredited do
+	role :user do
 		has_permission_on [:tickets], :to => [:index, :show, :new, :create, :edit, :update]
 		has_permission_on [:assets], :to => [:index]
 	end
