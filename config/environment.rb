6af540e0fd39175755ffe8e8592503fa8d21b165
erpel <philipp@copythat.de>
2010-02-19 12:01:59 +0100

    require searchlogic gem

--- config/environment.rb
+++ config/environment.rb
@@ -26,6 +26,7 @@ config.gem "fastercsv"
 config.gem "comma"
 config.gem "will_paginate"
 config.gem 'formtastic'
+config.gem 'searchlogic'
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
