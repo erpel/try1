a2471e406f9e7ee39e4f35ca20e4614b90a5fedc
erpel <philipp@copythat.de>
2010-02-25 22:17:36 +0100

    filterbutton in headerreihe

---
+++ public/javascripts/erpel.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+	$("#dateFilterTrigger").click(function () {
+		//$("#dateFilterSelects").show("fast");
+		$("#dateFilterSelects").css('display','table-row');
+		$("#dateFilterTrigger").hide("fast");
+	})
+});
