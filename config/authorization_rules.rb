2cebb3f382cf0ff8efebb0ceaae83e00e5e0215e
erpel <philipp@copythat.de>
2010-04-14 15:55:32 +0200

    access restriction for addresses and templates

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -1,15 +1,19 @@
 authorization do 
 	role :superuser do
 		has_permission_on [:import_assets], :to => [:index, :csv_import]
-		has_permission_on [:tickets,:assets, :roles, :infobits, :infocollections], :to => [:index, :show, :new, :create, :edit, :update, :destroy]
+		has_permission_on [:tickets,:assets, :roles, :infobits, :infocollections,
+		 :e_mail_texts, :data_center_addresses],
+		 :to => [:index, :show, :new, :create, :edit, :update, :destroy]
 	end
 	role :admin do
 		has_permission_on [:import_assets], :to => [:index, :csv_import]
-		has_permission_on [:tickets,:assets], :to => [:index, :show, :new, :create, :edit, :update, :destroy]
+		has_permission_on [:tickets,:assets, :e_mail_texts, :data_center_addresses],
+		 :to => [:index, :show, :new, :create, :edit, :update, :destroy]
 	end
 	
 	role :user do
-		has_permission_on [:tickets], :to => [:index, :show, :new, :create, :edit, :update]
+		has_permission_on [:tickets, :e_mail_texts, :data_center_addresses],
+		 :to => [:index, :show, :new, :create, :edit, :update]
 		has_permission_on [:assets], :to => [:index]
 	end
 
